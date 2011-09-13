812102af091db4a897842891baf3fb620e62029e
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-13 21:55:22 +0000

    * xth router rewrite: ** Less procedural logic, more class-vars ** Distinction between overall defaults and page defaults (from plugins list_type) ** Ability to find a suitable page id based on all plugins defaults from list_type (Use param 'gp:id' with local or 'id' with global route set to 'auto' for this)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/zfext/trunk@51926 735d13b6-9817-0410-8766-e36946ffe9aa

--- zfext/library/Zfext/Controller/Router/Typo3.php
+++ zfext/library/Zfext/Controller/Router/Typo3.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Zfext - Zend Framework for TYPO3
- * 
+ *
  * LICENSE
  *
  * This script is part of the TYPO3 project. The TYPO3 project is
@@ -19,7 +19,7 @@
  * GNU General Public License for more details.
  *
  * This copyright notice MUST APPEAR in all copies of the script!
- * 
+ *
  * @copyright  Copyright (c) 2010 Christian Opitz - Netzelf GbR (http://netzelf.de)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @version    $Id$
@@ -34,16 +34,88 @@
 class Zfext_Controller_Router_Typo3 extends Zend_Controller_Router_Abstract
 {
 	const GLOBALS_PATTERN = '/(?:gp|gpvar):(.+)/i';
-	
+
 	const LOCALS_PATTERN = '/my:(.+)/i';
-	
+
 	protected $_defaultRoute = 'local';
-	
+
 	/**
 	 * @var tslib_pibase
 	 */
 	protected $_plugin;
-    
+
+	/**
+	 * The select keys from tt_content records with this plugin
+	 * @var array
+	 */
+	protected $_selectKeys;
+
+    /**#@+
+     * Array keys to use for module, controller, and action. Should be taken out of request.
+     * @var string
+     */
+    protected $_moduleKey     = 'module';
+    protected $_controllerKey = 'controller';
+    protected $_actionKey     = 'action';
+    /**#@-*/
+
+    /**
+     * @var Zend_Controller_Dispatcher_Standard
+     */
+    protected $_dispatcher;
+
+    /**
+     * @var Zend_Controller_Request_Abstract
+     */
+    protected $_request;
+
+    /*
+     * @var array
+     */
+    protected $_defaults = array();
+
+    /**
+     * @var array
+     */
+    protected $_pageDefaults = null;
+
+
+    /**
+     * Constructor
+     *
+     * @param array $defaults Defaults for map variables with keys as variable names
+     * @param Zend_Controller_Dispatcher_Interface $dispatcher Dispatcher object
+     * @param Zend_Controller_Request_Abstract $request Request object
+     */
+    protected function _init(Zend_Controller_Request_Abstract $request, $plugin)
+    {
+        $front = Zend_Controller_Front::getInstance();
+	    $this->_dispatcher = $front->getDispatcher();
+	    $this->_request = $request;
+	    $this->_plugin = $plugin;
+
+        $this->_moduleKey     = $this->_request->getModuleKey();
+        $this->_controllerKey = $this->_request->getControllerKey();
+        $this->_actionKey     = $this->_request->getActionKey();
+
+        $this->_defaults += array(
+            $this->_controllerKey => $this->_dispatcher->getDefaultControllerName(),
+            $this->_actionKey     => $this->_dispatcher->getDefaultAction(),
+            $this->_moduleKey     => $this->_dispatcher->getDefaultModule()
+        );
+
+        $pluginOptions = Zfext_ExtMgm::getPluginOptions($this->_plugin->prefixId);
+        if (isset($pluginOptions['defaultModule'])) {
+            $this->_defaults[$this->_moduleKey] = $pluginOptions['defaultModule'];
+        }
+        if (isset($pluginOptions['defaultController'])) {
+            $this->_defaults[$this->_controllerKey] = $pluginOptions['defaultController'];
+        }
+        if (isset($pluginOptions['defaultAction'])) {
+            $this->_defaults[$this->_actionKey] = $pluginOptions['defaultAction'];
+        }
+    }
+
 	/**
      * Processes a request and sets its controller and action.  If
      * no route was possible, an exception is thrown.
@@ -55,87 +127,85 @@ class Zfext_Controller_Router_Typo3 extends Zend_Controller_Router_Abstract
 	public function route(Zend_Controller_Request_Abstract $request)
 	{
 		$request->clearParams();
-		
-	    $this->_plugin = Zfext_Plugin::getInstance();
-	    
-	    $front = Zend_Controller_Front::getInstance();
-	    $dispatcher = $front->getDispatcher();
-	    
-	    $options = array_merge(
-	        array(
-	        	'defaultModule' => $dispatcher->getDefaultModule(),
-	            'defaultController' => $dispatcher->getDefaultControllerName(),
-	            'defaultAction' => $dispatcher->getDefaultAction()
-	        ),
-	        Zfext_ExtMgm::getPluginOptions($this->_plugin->prefixId)
-	    );
-	    
+
+	    $this->_init($request, Zfext_Plugin::getInstance());
+
 	    $piVars = (array) $this->_plugin->piVars;
-	    
-	    $mKey = $request->getModuleKey();
-	    $cKey = $request->getControllerKey();
-	    $aKey = $request->getActionKey();
-	    
-	    // Detect if a startup-route was set in BE
-	    if (strlen($this->_plugin->cObj->getFieldVal('select_key'))) {
-	    	$parts = explode('/', $this->_plugin->cObj->getFieldVal('select_key'), 3);
-	    	$action = array_pop($parts);
-	    	$controller = array_pop($parts);
-	    	$module = array_pop($parts);
-	    	
-	    	if ((empty($piVars[$mKey]) || $piVars[$mKey] == $module) &&
-	    	    (empty($piVars[$cKey]) || $piVars[$cKey] == $controller)) {
-	    		$options['defaultAction'] = $action;
-	    	}
-	    	
-	    	if (empty($piVars[$mKey]) || $piVars[$mKey] == $module) {
-		    	$options['defaultController'] = $controller;
-	    	}
-	    	
-		    $options['defaultModule'] = $module;
+	    $defaults = $this->_getPageDefaults(0);
+	    $params = array_merge($defaults, $piVars);
+
+	    // Fix eventually non existing module<->controller and controller<->action combinations
+	    // We could also do this in assemblage but then we would have a lot longer urls
+	    if (isset($piVars[$this->_moduleKey]) && $piVars[$this->_moduleKey] != $defaults[$this->_moduleKey]) {
+	        if (!isset($piVars[$this->_controllerKey])) {
+	            $params[$this->_controllerKey] = $this->_defaults[$this->_controllerKey];
+	        }
 	    }
-	    
-	    // This could be done better but for now it's ok
-	    // set the determined m/c/a options back to frontcontroller
-	    $front->setDefaultModule($options['defaultModule']);
-	    $front->setDefaultControllerName($options['defaultController']);
-	    $front->setDefaultAction($options['defaultAction']);
-	    
-	    $params = array_merge(
-	        array (
-    	        $mKey => $options['defaultModule'],
-    	        $cKey => $options['defaultController'],
-    	        $aKey => $options['defaultAction']
-	        ),
-	        $piVars
-	    );
-	    
-	    foreach ($params as $param => $value)
-	    {
-	    	if ($param == $request->getModuleKey())
-	    	{
+	    if (isset($piVars[$this->_controllerKey]) && $piVars[$this->_controllerKey] != $defaults[$this->_controllerKey]) {
+	        if (!isset($piVars[$this->_actionKey])) {
+	            $params[$this->_actionKey] = $this->_defaults[$this->_actionKey];
+	        }
+	    }
+
+	    // Set params to the request
+	    foreach ($params as $param => $value) {
+	    	if ($param == $this->_moduleKey) {
 	    		$request->setModuleName($value);
 	    	}
-	        if ($param == $request->getControllerKey())
-	        {
+	        if ($param == $this->_controllerKey) {
 	            $request->setControllerName($value);
 	        }
-	        if ($param == $request->getActionKey())
-	        {
+	        if ($param == $this->_actionKey) {
 	            $request->setActionName($value);
 	        }
 	        $request->setParam($param, $value);
 	    }
-		
+
 		return $request;
 	}
-	
+
+	/**
+	 * Parse a path exactly like the module route in ZF
+	 *
+	 * @param string $path
+	 * @return array
+	 */
+	protected function _parsePath($path)
+	{
+        $values = array();
+        $params = array();
+	    $path = explode('/', trim($path, '/'));
+
+        if ($this->_dispatcher && $this->_dispatcher->isValidModule($path[0])) {
+            $values[$this->_moduleKey] = array_shift($path);
+            $this->_moduleValid = true;
+        }
+
+        if (count($path) && !empty($path[0])) {
+            $values[$this->_controllerKey] = array_shift($path);
+        }
+
+        if (count($path) && !empty($path[0])) {
+            $values[$this->_actionKey] = array_shift($path);
+        }
+
+        if ($numSegs = count($path)) {
+            for ($i = 0; $i < $numSegs; $i = $i + 2) {
+                $key = urldecode($path[$i]);
+                $val = isset($path[$i + 1]) ? urldecode($path[$i + 1]) : null;
+                $params[$key] = (isset($params[$key]) ? (array_merge((array) $params[$key], array($val))): $val);
+            }
+        }
+
+        return $values + $params;
+	}
+
 	/**
      * Generates a URL path that can be used in URL creation, redirection, etc.
      *
      * May be passed user params to override ones from URI, Request or even defaults.
      * If passed parameter has a value of null, the current URL will be returned.
-     * 
+     *
      * You can use namespaces in the param-names (array-keys of the params-array).
      * These namespaces decide if the param is a global ($_GET) or local (piVars)
      * param, independent from route. See example below.
@@ -148,12 +218,20 @@ class Zfext_Controller_Router_Typo3 extends Zend_Controller_Router_Abstract
      * ));
      * //Returns: index.php?id=3&tx_example[id]=3
      * </code>
-     * 
+     *
      * There are two routes available: "local" and "global". The difference is where
      * the vars that were not filtered out by namespace (see above) will be put:
      * - Local route puts them to the local params (piVars)
-     * - Global route puts them to the global params ($_GET)  
+     * - Global route puts them to the global params ($_GET)
      * If null is passed as a route name assemble will use $_defaultRoute (local).
+     *
+     * Also there is a pseudo route called xhr which generates urls to the ZFext-
+     * eID script. You can also prefix the above routes with xhr (eg. "xhr.global")
+     *
+     * Another usefull feature is that the router can find a suitable FE-ID for you
+     * when you set gp:id-param to 'auto' (or the 'id'-param with the global route).
+     * Therefore it searches for plugin-rows with a suitable select_key (where you
+     * can setup the default path) - @see _findPageId()
      *
      * Reset is used to signal that all parameters should be reset to it's defaults.
      * Ignoring all URL specified values. User specified params still get precedence.
@@ -175,7 +253,7 @@ class Zfext_Controller_Router_Typo3 extends Zend_Controller_Router_Abstract
             $userParams['gp:eID'] = 'zfext';
             $userParams['gp:tx_zfext'] = array('eid' => $this->_plugin->prefixId);
         }
-        
+
         if (!count($userParams)) {
             if ($reset) {
                 $this->_plugin->pi_linkTP('|');
@@ -184,14 +262,14 @@ class Zfext_Controller_Router_Typo3 extends Zend_Controller_Router_Abstract
                 return $this->_plugin->pi_linkTP_keepPIvars_url();
             }
         }
-        
+
         $route = (is_string($name) && strlen($name) && $name != 'default') ? strtolower($name) : $this->_defaultRoute;
-        
+
         $prefixId = $this->_plugin->prefixId;
-        
+
         $globalParams = array();
-               
-        if (isset($userParams[$prefixId]) && 
+
+        if (isset($userParams[$prefixId]) &&
             is_array($userParams[$prefixId]))
         {
             $localParams = $userParams[$prefixId];
@@ -199,7 +277,7 @@ class Zfext_Controller_Router_Typo3 extends Zend_Controller_Router_Abstract
         } else {
             $localParams = array();
         }
-        
+
         //Filter the params:
         foreach ($userParams as $param => $value) {
         	if (preg_match(self::GLOBALS_PATTERN, $param, $matches)) {
@@ -215,65 +293,139 @@ class Zfext_Controller_Router_Typo3 extends Zend_Controller_Router_Abstract
         	    $localParams[$param] = $value;
         	}
         }
-        
-        $altPageId = 0;
-        $cache = 0;
-        
-        if (!empty($globalParams['id'])) {
-            $altPageId = $globalParams['id'];
-            unset($globalParams['id']);
-        }
-        
+
         if (!$reset) {
             $getParams = $_GET;
             unset($getParams[$prefixId], $getParams['id'],$getParams['eID'], $getParams['tx_zfext']);
             $globalParams = $this->_mergeParams($getParams, $globalParams);
             $localParams = $this->_mergeParams((array) $this->_plugin->piVars, $localParams);
-            
+
             if ($this->_plugin->pi_autoCacheEn) {
                 $cache = $this->_plugin->pi_autoCache($localParams);
             }
         }
         unset($localParams['DATA']);
-        
-        $front = Zend_Controller_Front::getInstance();
-        $request = $front->getRequest();
-        
-        // Filter out unnecessary keys - but only on this site:
-        if ($altPageId == $GLOBALS['TSFE']->id) {
-            $mKey = $request->getModuleKey();
-            $cKey = $request->getControllerKey();
-            $aKey = $request->getActionKey();
-            
-            if (isset($localParams[$mKey]) && $localParams[$mKey] == $front->getDefaultModule()) {
-                unset($localParams[$mKey]);
-                if (isset($localParams[$cKey]) && $localParams[$cKey] == $front->getDefaultControllerName()) {
-                    unset($localParams[$cKey]);
-                    if (isset($localParams[$aKey]) && $localParams[$aKey] == $front->getDefaultAction()) {
-                        unset($localParams[$aKey]);
+
+        $altPageId = 0;
+        $cache = 0;
+
+        if (!empty($globalParams['id'])) {
+            if ($globalParams['id'] == 'auto') {
+                $altPageId = $this->_findPageId($localParams);
+            } else {
+                $altPageId = $globalParams['id'];
+            }
+            unset($globalParams['id']);
+        }
+
+        $globalParams[$prefixId] = array_diff($localParams, $this->_getPageDefaults($altPageId));
+
+        $this->_plugin->pi_linkTP('|', $globalParams, $cache, $altPageId);
+
+        return $this->_plugin->cObj->lastTypoLinkUrl;
+    }
+
+    /**
+     * Attempts to find a suitable id for the params
+     *
+     * @param array $params
+     * @return int
+     */
+    protected function _findPageId($params)
+    {
+        $params = array_merge($this->_defaults, $params);
+        $module = $params[$this->_moduleKey];
+        $controller = $params[$this->_controllerKey];
+        $action = $params[$this->_actionKey];
+
+        $lookUp = array(
+            array(
+                $this->_moduleKey => $module,
+                $this->_controllerKey => $controller,
+                $this->_actionKey => $action
+            ),
+            array(
+                $this->_moduleKey => $module,
+                $this->_controllerKey => $controller
+            ),
+            array(
+                $this->_moduleKey => $module
+            ),
+            array(
+                $this->_controllerKey => $controller,
+                $this->_actionKey => $action
+            ),
+            array(
+                $this->_controllerKey => $controller
+            )
+        );
+
+
+        $allPageDefaults = $this->_getPageDefaults();
+        foreach ($lookUp as $i => $values) {
+            foreach ($allPageDefaults as $pid => $defaults) {
+                $match = 0;
+                foreach ($values as $key => $value) {
+                    if ($defaults[$key] == $value) {
+                        $match++;
                     }
+                }
+                if ($match == count($values)) {
+                    return $pid;
                 }
             }
         }
-        
-        $globalParams[$prefixId] = $localParams;
-        
-        $this->_plugin->pi_linkTP('|', $globalParams, $cache, $altPageId);
-        
-        return $this->_plugin->cObj->lastTypoLinkUrl;
+
+        return 0;
     }
-    
+
+    /**
+     * Gets the defaults for the pages where content elements with current
+     * type are contained.
+     *
+     * @param int|null $id
+     * @return array Defaults for a page or all defaults when $id is null
+     */
+    protected function _getPageDefaults($id = null)
+    {
+        if (!$this->_pageDefaults) {
+            $where = 'deleted = 0 AND hidden=0 AND CType=\'list\' AND ';
+            $where .= 'list_type = \''.$this->_plugin->cObj->getFieldVal('list_type').'\' AND select_key <> \'\'';
+
+            if (strlen($this->_plugin->cObj->getFieldVal('select_key'))) {
+    	    	$path = $this->_plugin->cObj->getFieldVal('select_key');
+    	    	$this->_pageDefaults[0] = array_merge($this->_defaults, $this->_parsePath($path));
+    	    	$this->_pageDefaults[$GLOBALS['TSFE']->id] = $this->_pageDefaults[0];
+    	    	$where .= ' AND pid <> '.$GLOBALS['TSFE']->id;
+    	    }
+
+            $rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('pid, select_key', 'tt_content', $where);
+            foreach ($rows as $row) {
+                $this->_pageDefaults[$row['pid']] = array_merge(
+                    $this->_defaults,
+                    $this->_parsePath($row['select_key'])
+                );
+            }
+        }
+        if ($id === null) {
+            return $this->_pageDefaults;
+        }
+        if (!isset($this->_pageDefaults[$id])) {
+            return $this->_defaults;
+        }
+        return $this->_pageDefaults[$id];
+    }
+
     /**
      * Wrapper to recursively merge arrays
-     * 
+     *
      * @param array $params1
      * @param array $params2
      * @return array
      */
     protected function _mergeParams($params1, $params2)
     {
-        if (!count($params2))
-        {
+        if (!count($params2)) {
             return $params1;
         }
         return t3lib_div::array_merge_recursive_overrule($params1, $params2);
